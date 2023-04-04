56d97b65388fd139abb75a30d3c2a043f596ab12
adorabledeadsystem <ezepchukk@mail.ru>
2023-04-04 16:54:19 +0300

    Equipment photos file download

--- src/components/Equipment.jsx
+++ src/components/Equipment.jsx
@@ -4,6 +4,10 @@ import scene from '../assets/scene.svg'
 import light from '../assets/light.svg'
 import equipment from '../assets/equipment.svg'
 import file from '../assets/Price.pdf'
+import fileSound from '../assets/Sound.pdf'
+import fileLight from '../assets/Light.pdf'
+import fileScene from '../assets/Scene.pdf'
+import fileEquipment from '../assets/Equipment.pdf'
 
 export default function Equipment() {
   return (
@@ -22,15 +26,30 @@ export default function Equipment() {
             </span>
           </p>
           <div className="equipment__photos">
-            <img src={sound} alt="sound" width={396} height={384} />
-            <img src={scene} alt="scene" width={396} height={384} />
-            <img src={light} alt="light" width={396} height={384} />
-            <img
-              src={equipment}
-              alt="equipment"
-              width={396}
-              height={384}
-            />
+            <div className="img__link">
+              <img src={sound} alt="sound" height={384}/>
+              <span className="img__mask">
+                <a className="download Monrat400" href={fileSound} download="Sound.pdf" >Скачать</a>
+              </span>
+            </div>
+            <div className="img__link">
+              <img src={scene} alt="sound"  height={384}/>
+              <span className="img__mask">
+                <a className="download Monrat400" href={fileScene} download="Scene.pdf" >Скачать</a>
+              </span>
+            </div>
+            <div className="img__link">
+              <img src={light} alt="sound" height={384}/>
+              <span className="img__mask">
+                <a className="download Monrat400" href={fileLight} download="Light.pdf" >Скачать</a>
+              </span>
+            </div>
+            <div className="img__link">
+              <img src={equipment} alt="sound" height={384} />
+              <span className="img__mask">
+                <a className="download Monrat400" href={fileEquipment} download="Equipment.pdf" >Скачать</a>
+              </span>
+            </div>
           </div>
         </div>
       </div>
